c641546a4f3f8ed53d8cd5f1a5816bf90232e84b
MLior8dCode <elmeddin.mohubbetov2@gmail.com>
2025-06-29 22:10:02 +0400

    TemperatureConverter.kt

--- src/main/kotlin/com/motycka/edu/lesson04/TemperatureConverter.kt
+++ src/main/kotlin/com/motycka/edu/lesson04/TemperatureConverter.kt
@@ -1,23 +1,8 @@
 package com.motycka.edu.lesson04
 
-import io.github.oshai.kotlinlogging.KotlinLogging
-
-private val logger = KotlinLogging.logger { }
-
 object TemperatureConverter {
-    fun toCelsius(fahrenheit: Double): Double {
-        logger.info { "Converting $fahrenheit Fahrenheit to Celsius" }
-        return (fahrenheit - 32) * 5 / 9
-    }
-    fun toFahrenheit(celsius: Double): Double {
-        logger.info { "Converting $celsius Celsius to Fahrenheit" }
-        return celsius * 9 / 5 + 32
-    }
 
-}
-
-
-fun main() {
-    TemperatureConverter.toFahrenheit(34.0).also(::println)
-    TemperatureConverter.toCelsius(94.0).also(::println)
+    fun toFahrenheit(celsius: Double): Double = celsius * 9.0 / 5.0 + 32.0
+    
+    fun toCelsius(fahrenheit: Double): Double = (fahrenheit - 32.0) * 5.0 / 9.0
 }
