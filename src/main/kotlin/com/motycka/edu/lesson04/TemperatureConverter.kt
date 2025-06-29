33f14e152e5a01c9829bdc92ded701bfca3f564e
MLior8dCode <elmeddin.mohubbetov2@gmail.com>
2025-06-29 22:16:46 +0400

    TemperatureConverter.kt

--- src/main/kotlin/com/motycka/edu/lesson04/TemperatureConverter.kt
+++ src/main/kotlin/com/motycka/edu/lesson04/TemperatureConverter.kt
@@ -2,7 +2,9 @@ package com.motycka.edu.lesson04
 
 object TemperatureConverter {
 
-    fun toFahrenheit(celsius: Double): Double = celsius * 9.0 / 5.0 + 32.0
-    
-    fun toCelsius(fahrenheit: Double): Double = (fahrenheit - 32.0) * 5.0 / 9.0
+    fun toFahrenheit(celsius: Double): Double =
+        celsius * 9.0 / 5.0 + 32.0
+
+    fun toCelsius(fahrenheit: Double): Double =
+        (fahrenheit - 32.0) * 5.0 / 9.0
 }
